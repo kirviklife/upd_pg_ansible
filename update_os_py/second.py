40aed9e9d9ecd2b081d4a48759f02fa6e2fd2ed8
Golovlev Kirill <kirviklife@gmail.com>
2025-05-01 19:33:20 +0300

    test

--- update_os_py/second.py
+++ update_os_py/second.py
@@ -1,7 +1,7 @@
 import sys
 arr = eval(sys.argv[1])
-# for item in arr:
-#     print('id: %s\tval1: %s\tval2: %s'%(item['id'],item['val1'],item['val2']))
+for item in arr:
+    print('id: %s\tval1: %s\tval2: %s'%(item['id'],item['val1'],item['val2']))
 
 print(arr)
 
