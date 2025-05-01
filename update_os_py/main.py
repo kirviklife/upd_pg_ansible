e0bb832ab87c79de9c392c2da8d925240e2cb95f
Golovlev Kirill <kirviklife@gmail.com>
2025-05-01 19:14:05 +0300

    test

--- update_os_py/main.py
+++ update_os_py/main.py
@@ -1,6 +1,3 @@
-import sys
 
-print([0,1,2,3])
-arr = eval(sys.argv[1])
-for item in arr:
-    print('id: %s\tval1: %s\tval2: %s'%(item['id'],item['val1'],item['val2']))
+
+print(0,[{'id': '0','val1': 2,'val2': 4},{'id': '1','val1': 4,'val2': 66}])
